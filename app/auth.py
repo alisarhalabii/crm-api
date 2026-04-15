25eff798c0b437a7db3a59078c3a2e851a15406f
alisarhalabii <alisarhalabii@gmail.com>
2026-04-15 12:23:11 +0200

    created dependency auth check so that only login users can access by demanding JWT token

--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,9 @@
 from datetime import datetime, timedelta
-from jose import jwt
+from jose import jwt, JWTError
 from passlib.context import CryptContext
-
+from fastapi import Depends
+from fastapi.security import HTTPBearer
+from fastapi import HTTPException
 
 
 SECRET_KEY= "supersecretkey" # becomes env variable
@@ -26,3 +28,15 @@ def create_access_token(data: dict):
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm= ALGORITHM)
+
+
+# Creating dependency auth check
+security= HTTPBearer()
+
+def get_current_user(token=Depends(security)):
+    try:
+        payload= jwt.decode(token.credentials, SECRET_KEY, algorithms=[ALGORITHM])
+        username= payload.get("sub")
+        return username
+    except JWTError:
+        raise HTTPException(status_code=401, detail= "Invalid token")
