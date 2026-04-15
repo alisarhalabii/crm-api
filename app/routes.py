2ed2f339d3281e11d8054bc33af3cda4dddaff89
alisarhalabii <alisarhalabii@gmail.com>
2026-04-15 13:19:56 +0200

    fixed auth bugz

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from .models import CustomerDB
 from .database import SessionLocal
+from .auth import get_current_user
 from pydantic import BaseModel
 
 
@@ -12,7 +13,7 @@ class Customer(BaseModel):
     status: str #leads, in contact, customer
 
 @router.post("/")
-def create_customer(customer: Customer):
+def create_customer(customer: Customer, user=Depends(get_current_user)):
     database= SessionLocal()
     db_customer= CustomerDB(**customer.model_dump()) #model_dump() instead of dict() because dict is deprecated
     database.add(db_customer)
@@ -23,16 +24,15 @@ def create_customer(customer: Customer):
 
 
 @router.get("/")
-def get_customers():
+def get_customers(user=Depends(get_current_user)):
     db= SessionLocal()
     customers = db.query(CustomerDB).all()
     db.close()
-    
     return customers
    #return {"message": "HELLO ALISAR"} --> Testing respons for direct get function in fastapi
 
 @router.put("/{customer_id}")
-def update_customer(customer_id: int, customer: Customer):
+def update_customer(customer_id: int, customer: Customer, user=Depends(get_current_user)):
     db= SessionLocal()
     db_customer= db.query(CustomerDB).filter(CustomerDB.id == customer_id).first()
     if not db_customer:
@@ -50,7 +50,7 @@ def update_customer(customer_id: int, customer: Customer):
     return db_customer
 
 @router.delete("/{customer_id}")
-def delete_customer(customer_id: int):
+def delete_customer(customer_id: int, user=Depends(get_current_user)):
     db = SessionLocal()
     db_customer = db.query(CustomerDB).filter(CustomerDB.id == customer_id).first()
     
