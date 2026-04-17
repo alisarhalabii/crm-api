749903d822f9567469cb4d494cd12ff955259c0a
alisarhalabii <alisarhalabii@gmail.com>
2026-04-17 10:02:03 +0200

    fixing bugs

--- app/main.py
+++ app/main.py
@@ -3,15 +3,29 @@ from .routes import router
 from .database import engine
 from .models import Base 
 from .auth_routes import auth_router
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
 
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# DB
 Base.metadata.create_all(bind=engine)
-app= FastAPI()
 
+# Routers
 app.include_router(router)
 app.include_router(auth_router)
 
+# Root
 @app.get("/") 
 def root(): 
-    return {"message": "CRM API is live"} 
-
+    return {"message": "CRM API is live"}
